6d4722cb970bb76bb315db1597f201c6f307d9bc
kiramario <645364525@qq.com>
2017-04-06 00:25:11 +0800

    20170406 modify

--- fys/craw.py
+++ fys/craw.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-import urllib, urllib2, _winreg, re
+import urllib, urllib2, _winreg, re, os
 from bs4 import BeautifulSoup,NavigableString
 
 class Crawler(object):
@@ -30,8 +30,9 @@ class Crawler(object):
 			print e.reason
 
 		
-	def doWriteIntxt(self,restring=None):
-		txtfile = self.get_desktop() + '\\resText.txt'
+	def doWriteIntxt(self,restring=None, txtfile=None):
+		if not txtfile:
+			txtfile = self.get_desktop() + '\\__default_File.txt'
 		if restring:
 			try:
 				f = open(txtfile,'w+')
@@ -40,11 +41,39 @@ class Crawler(object):
 				print e
 			finally:
 				f.close()
+				
+	def makedir(self,dirname):
+		finaldirname = self.get_desktop() + dirname
+		if not os.path.exists(finaldirname):
+			os.makedirs(finaldirname)
+		
+		return finaldirname
 		
 	def get_desktop(self):
 		key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
 		return _winreg.QueryValueEx(key, "Desktop")[0]
 		
+		
+class DocCraw(Crawler):
+	httpaddr = "http://www.spprec.com"
+	def __init__(self,docname):
+		self.docname = docname
+		pass
+
+	def analyse(self,results):
+		soup = BeautifulSoup(results)
+		div = soup.find("div",id="main")
+		
+		txtfile = self.makedir('\\fanyongsheng\\html') + '\\' + self.docname + '.html'
+			
+		self.doWriteIntxt(unicode(div).encode('gb18030'),txtfile)
+		
+	def run(self):
+		url = DocCraw.httpaddr + "/sczw/InfoDetail/Default.aspx?InfoID=f3f3613b-b370-4e9c-a31e-69c2bbd98fa0&CategoryNum=005001001"
+		results = self.doSearch(url=url,data=None,headers=None)
+		self.analyse(results)
+
+
 class Fanyongsheng(Crawler):
 	httpaddr = "http://www.spprec.com"
 	outputstr = ''
@@ -59,6 +88,15 @@ class Fanyongsheng(Crawler):
 		outputstr = outputstr + "\r\n" + Fanyongsheng.httpaddr + aTag['href'] + "\r\n"
 		outputstr = outputstr + unicode(aTag.string) + "\r\n"
 		outputstr = outputstr + unicode(time) + "\r\n"
+		
+		
+		pat = re.compile('InfoID=(.*)&')
+		se = pat.search(aTag['href'])
+		htmlfilename = se.group(1)
+	
+		#抓取doc
+		d = DocCraw(htmlfilename)
+		d.run()	
 		return outputstr
 		
 	def analyseTr(self, trTag):
@@ -78,8 +116,9 @@ class Fanyongsheng(Crawler):
 	def run(self):
 		data = {"__VIEWSTATE": '',"__EVENTTARGET":"MoreInfoList1$Pager","__EVENTARGUMENT":'1'}
 		url = Fanyongsheng.httpaddr + "/sczw/jyfwpt/005001/005001001/MoreInfo.aspx?CategoryNum=005001001"
-		results = self.doSearch(url=url,data=None,headers=None)
+		results = self.doSearch(url=url,data=data,headers=None)
 		self.analyse(results)
 		
 f = Fanyongsheng()
 f.run()
+
